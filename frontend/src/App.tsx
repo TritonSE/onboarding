6399fecf9e3ed2360035e60885ac4c072170285e
Navid Boloorian <navidboloorian0@gmail.com>
2023-07-20 13:42:42 -0700

    react boilerplate added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
