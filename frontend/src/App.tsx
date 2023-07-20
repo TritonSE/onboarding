1935849bef6cdac0c412360b5ff223c83527728c
Navid Boloorian <navidboloorian0@gmail.com>
2023-07-20 16:55:57 -0700

    added frontend linter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
