e4064370667b78d97a7a87a520a90669848aefd6
William Wu <dev.wwu@gmail.com>
2023-07-26 20:37:18 -0700

    Set `Button` height so box-sizing works

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Page, TextField } from "src/components/";
+import { Button, Page, TextField } from "src/components/";
 
 export default function Home() {
   return (
     <Page>
       <h1>Home</h1>
       <TextField label="Asdf" />
+      <Button label="Click me" style={{ marginTop: 4 }} />
+      <Button label="Click me too" kind="secondary" style={{ marginTop: 4, marginLeft: 4 }} />
     </Page>
   );
 }
