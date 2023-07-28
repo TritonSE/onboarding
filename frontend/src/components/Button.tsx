14924678b1b94be81573d47d5f2b5bd1dce88329
William Wu <74469686+wllmwu@users.noreply.github.com>
2023-07-28 11:15:45 -0700

    Add basic frontend components (#8)
    * Update config files to use path aliases
    
    * Install `react-helmet`, add global stylesheet and Rubik font
    
    * Set execution permission on pre-commit hook
    
    * Add `Button` component
    
    * Update `Button`, relocate CSS files
    
    * Add `TextField` component
    
    * Fix CSS import paths
    
    * Add `eslint-plugin-no-relative-import-paths`
    
    * Add `HeaderBar` component
    
    * Add `Page` component
    
    * Set `Button` height so box-sizing works
    
    * Add comments

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from "src/components/Button.module.css";
+
+/**
+ * An interface defines the shape of an object (what fields are expected to exist and their types):
+ * https://www.typescriptlang.org/docs/handbook/2/objects.html.
+ *
+ * We can use interfaces to specify the types of a component's props. For this `Button` component,
+ * we require a string prop `label` and an optional prop `kind` which is restricted to two possible
+ * string values: "primary" or "secondary". The interface also extends another interface,
+ * `React.ComponentProps<"button">`, which comes from React and means that we also want to allow
+ * all the props that a regular `<button>` component can receive (such as `onPress`). This is only
+ * really useful for super basic "atom" components---most higher-level ones should have a pretty
+ * limited, explicitly defined set of props.
+ */
+export interface ButtonProps extends React.ComponentProps<"button"> {
+  label: string;
+  kind?: "primary" | "secondary";
+}
+
+/**
+ * This `Button` is pretty straightforward, besides the use of `React.forwardRef`. A ref is a way to
+ * store a reference to a component so you can do things with it in effects and other special cases
+ * (see https://react.dev/learn/manipulating-the-dom-with-refs). All built-in React components
+ * can accept refs, but components that we write cannot unless we use this `forwardRef` function.
+ * It wraps around the regular functional component and provides us with the `ref` argument which we
+ * pass along to the actual `<button>` element. If a developer chooses to use a ref on our `Button`,
+ * then it works as if it's on the `<button>` itself. If they don't use a ref, then nothing special
+ * happens.
+ *
+ * Also, note that we pass all the rest of the props (`...props`, called "rest parameter syntax") to
+ * the `<button>`. Thus our `Button` component works mostly like a standard `<button>` element, but
+ * with our own styling and restrictions on what can be put inside of it.
+ */
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+  { label, kind = "primary", ...props },
+  ref,
+) {
+  let className = styles.button;
+  switch (kind) {
+    case "primary":
+      className += ` ${styles.primary}`;
+      break;
+    case "secondary":
+      className += ` ${styles.secondary}`;
+      break;
+  }
+  return (
+    <button ref={ref} className={className} {...props}>
+      {label}
+    </button>
+  );
+});
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Button, Page, TextField } from "src/components/";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Page>
+      <h1>Home</h1>
+      <TextField label="Asdf" />
+      <Button label="Click me" style={{ marginTop: 4 }} />
+      <Button label="Click me too" kind="secondary" style={{ marginTop: 4, marginLeft: 4 }} />
+    </Page>
+  );
 }
