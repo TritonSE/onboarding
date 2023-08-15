31b79b87a6c7cc43c1c43b495313dfd2ada24c8d
Navid Boloorian <navidboloorian0@gmail.com>
2023-08-14 22:41:49 -0700

    create task frontend api initialized

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Button, Page, TextField } from "src/components/";
+import * as TaskApi from "src/api/task_api";
 
 export default function Home() {
+  const exp = async () => {
+    const resp = await TaskApi.createTask({'title': 'test', 'description': 'test'});
+  }
+
   return (
     <Page>
       <h1>Home</h1>
       <TextField label="Asdf" />
-      <Button label="Click me" style={{ marginTop: 4 }} />
-      <Button label="Click me too" kind="secondary" style={{ marginTop: 4, marginLeft: 4 }} />
+      <Button label="Click me" style={{ marginTop: 4 }} onClick={async () => await exp()}/>
+      <Button label="Click me too" kind="secondary" onClick={() => alert("FIRED")} style={{ marginTop: 4, marginLeft: 4 }} />
     </Page>
   );
 }
