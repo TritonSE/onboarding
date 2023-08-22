7184f38a33b9c1ca30ce65d60e6273a8929f34db
navidboloorian <59907006+navidboloorian@users.noreply.github.com>
2023-08-21 19:59:51 -0700

    Implement frontend API client (#13)
    * refactored requests
    
    * added task api with createTask implemented
    
    * updated comments
    
    * updated comments
    
    * removed CORS/proxy comment
    
    * Resolve hidden comments
    
    ---------
    
    Co-authored-by: William Wu <dev.wwu@gmail.com>

---
+++ frontend/src/api/tasks.ts
@@ -0,0 +1,69 @@
+import { post } from "src/api/requests";
+
+/**
+ * Defines the "shape" of a Task object (what fields are present and their types) for
+ * frontend components to use. This will be the return type of most functions in this
+ * file.
+ */
+export interface Task {
+  _id: string;
+  title: string;
+  description?: string;
+  isChecked: boolean;
+  dateCreated: Date;
+}
+
+/**
+ * Defines the shape of JSON that we'll receive from the backend when we ask the API
+ * for a Task object. That is, when the backend sends us a JSON object representing a
+ * Task, we expect it to match these fields and types.
+ *
+ * The difference between this type and `Task` above is that `dateCreated` is a string
+ * instead of a Date object. This is because JSON doesn't support Dates, so we use a
+ * date-formatted string in requests and responses.
+ */
+interface TaskJSON {
+  _id: string;
+  title: string;
+  description?: string;
+  isChecked: boolean;
+  dateCreated: string;
+}
+
+/**
+ * Converts a Task from JSON that only contains primitive types to our custom
+ * Task interface.
+ *
+ * @param task The JSON representation of the task
+ * @returns The parsed Task object
+ */
+function parseTask(task: TaskJSON): Task {
+  return {
+    _id: task._id,
+    title: task.title,
+    description: task.description,
+    isChecked: task.isChecked,
+    dateCreated: new Date(task.dateCreated),
+  };
+}
+
+/**
+ * The expected inputs when we want to create a new Task object. In the MVP, we only
+ * need to provide the title and optionally the description, but in the course of
+ * this tutorial you'll likely want to add more fields here.
+ */
+export interface CreateTaskRequest {
+  title: string;
+  description?: string;
+}
+
+export async function createTask(task: CreateTaskRequest): Promise<Task> {
+  const response = await post("/api/task", task);
+  const json = (await response.json()) as TaskJSON;
+
+  return parseTask(json);
+}
+
+export async function getAllTasks(): Promise<Task[]> {
+  return [];
+}
