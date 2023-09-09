d84c489ab0de25350510adc458c0988234054ca2
William Wu <dev.wwu@gmail.com>
2023-09-09 15:20:07 -0700

    Add page titles, remove `@types/react-helmet`

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 import { Page } from "src/components";
 
 export function About() {
   return (
     <Page>
+      <Helmet>
+        <title>About | TSE Todos</title>
+      </Helmet>
       <p>This is a simple example app that demonstrates TSE&apos;s software development process.</p>
       <p>
         <Link to="/">Back to home</Link>
