5abffc5cc49b0ff14a265b851a7f4ce25b03dfb7
William Wu <74469686+wllmwu@users.noreply.github.com>
2023-09-09 16:46:05 -0700

    Add site favicon (#23)
    * Add favicon files, move <head> code to `index.html`
    
    * Add page titles, remove `@types/react-helmet`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet-async";
 import { About, Home } from "src/pages";
 import "src/globals.css";
 
 export default function App() {
   return (
     <HelmetProvider>
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
