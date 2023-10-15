989c0e438c6eaa32023915b859c7719bb2c1c527
William Wu <dev.wwu@gmail.com>
2023-10-15 09:36:21 -0700

    Add `CheckButton` component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,6 +1,7 @@
 // the no-relative-import-paths eslint plugin doesn't check exports
 // IMO this is fine for an index file
 export { Button } from "./Button";
+export { CheckButton } from "./CheckButton";
 export { HeaderBar } from "./HeaderBar";
 export { Page } from "./Page";
 export { TaskForm } from "./TaskForm";
