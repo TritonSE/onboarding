9a333e96e0d30c6e1bd1acd2087cb41598e5d302
William Wu <dev.wwu@gmail.com>
2023-10-15 11:46:52 -0700

    Improve backend comments

--- backend/src/controllers/task.ts
+++ backend/src/controllers/task.ts
@@ -8,18 +8,37 @@ import { validationResult } from "express-validator";
 import TaskModel from "src/models/task";
 import validationErrorParser from "src/util/validationErrorParser";
 
+/**
+ * This is an example of an Express API request handler. We'll tell Express to
+ * run this function when our backend receives a request to retrieve a
+ * particular task.
+ *
+ * Request handlers typically have 3 parameters: req, res, and next.
+ *
+ * @param req The Request object from Express. This contains all the data from
+ * the API request. (https://expressjs.com/en/4x/api.html#req)
+ * @param res The Response object from Express. We use this to generate the API
+ * response for Express to send back. (https://expressjs.com/en/4x/api.html#res)
+ * @param next The next function in the chain of middleware. If there's no more
+ * processing we can do in this handler, but we're not completely done handling
+ * the request, then we can pass it along by calling next(). For all of the
+ * handlers defined in `src/controllers`, the next function is the global error
+ * handler in `src/app.ts`.
+ */
 export const getTask: RequestHandler = async (req, res, next) => {
   const { id } = req.params;
 
   try {
-    // if the id doesn't exist, returns null
+    // if the ID doesn't exist, then findById returns null
     const task = await TaskModel.findById(id);
 
     if (task === null) {
-      // 404 is the "not found" error
       throw createHttpError(404, "Task not found.");
     }
 
+    // Set the status code (200) and body (the task object as JSON) of the response.
+    // Note that you don't need to return anything, but you can still use a return
+    // statement to exit the function early.
     res.status(200).json(task);
   } catch (error) {
     // pass errors to the error handler
@@ -28,11 +47,12 @@ export const getTask: RequestHandler = async (req, res, next) => {
 };
 
 export const createTask: RequestHandler = async (req, res, next) => {
-  // fetch errors returned by the validator
+  // extract any errors that were found by the validator
   const errors = validationResult(req);
   const { title, description, isChecked } = req.body;
 
   try {
+    // if there are errors, then this function throws an exception
     validationErrorParser(errors);
 
     const task = await TaskModel.create({
--- backend/src/models/task.ts
+++ backend/src/models/task.ts
@@ -4,6 +4,11 @@ const taskSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
   isChecked: { type: Boolean, default: false },
+  // Note that dateCreated has type Date, which is MongoDB's recommended format
+  // for storing dates (as opposed to, say, strings or numbers--see
+  // https://www.mongodb.com/developer/products/mongodb/bson-data-types-date/).
+  // When we send a Task object in the JSON body of an API response, the date
+  // will automatically get "serialized" into a standard date string.
   dateCreated: { type: Date, required: true },
 });
 
