c39d258d59dbad5405a543c202a93f89812ffd73
Navid Boloorian <navidboloorian0@gmail.com>
2023-11-02 23:45:53 -0700

    added proxy middlware

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+import createProxyMiddleware from "http-proxy-middleware";
+
+export default function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:3001",
+      changeOrigin: true,
+    }),
+  );
+}
