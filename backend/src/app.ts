d4118b81093ef2b8a6bcca327f6ef35ddbb8fe4a
William Wu <dev.wwu@gmail.com>
2023-11-03 14:07:39 -0700

    Make a better fix with env vars and CORS headers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,13 +4,25 @@
 
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
 import { isHttpError } from "http-errors";
 import taskRoutes from "src/routes/task";
 
 const app = express();
 
-// initializes express to accept json in the request/response body
+// initializes Express to accept JSON in the request/response body
 app.use(express.json());
+
+// sets the "Access-Control-Allow-Origin" header on all responses to allow
+// requests from the frontend, which has a different origin - see the following
+// pages for more info:
+// https://expressjs.com/en/resources/middleware/cors.html
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+app.use(
+  cors({
+    origin: process.env.FRONTEND_ORIGIN,
+  }),
+);
 
 app.use("/api/task", taskRoutes);
 
@@ -19,11 +31,9 @@ app.use("/api/task", taskRoutes);
  * Explicit typings required here because TypeScript cannot infer the argument types.
  *
  * An eslint-disable is being used below because the "next" argument is never used. However,
- * it is still required for express to recognize it as an error handler. For this reason, I've
+ * it is still required for Express to recognize it as an error handler. For this reason, I've
  * disabled the eslint error. This should be used sparingly and only in situations where the lint
- * error cannot be fixed in another way. Alternatively, the variable name can be prefixed with an
- * underscore (i.e. _next). Use underscores for variables that are unused but are still required.
- * In this case, eslint-disable is used for educational purposes.
+ * error cannot be fixed in another way.
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
